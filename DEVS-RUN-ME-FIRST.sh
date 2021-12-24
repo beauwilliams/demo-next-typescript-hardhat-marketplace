9274766398b071f2fabe188ac74e1ea66235bae8
beauwilliams <beau.williams@icloud.com>
2021-12-24 16:51:07 +1100

    finalise deploy script

--- DEVS-RUN-ME-FIRST.sh
+++ DEVS-RUN-ME-FIRST.sh
@@ -29,6 +29,6 @@ if [[ $REPLY =~ [yY](es)* ]] || [[ -z "$REPLY" ]]; then
     echo "starting the servers and compiling contracts for first time... visit localhost:3000 to view site"
     sleep 1
     ./start.sh
-    echo "$(tput setaf 2)Local deployment complete. Visit localhost:3000 to view the front end"
-
 fi
+
+echo "$(tput setaf 2)Initial set up is complete. You can now use 'start' 'stop' 'test' commands. .......Re run this script to set up aliases on a new shell by following prompts."
--- start.sh
+++ start.sh
@@ -43,3 +43,5 @@ tmux2pane servers "npx next dev -p 3000" "npx hardhat node"
 sleep 5
 echo "compiling contracts..."
 npx hardhat run scripts/deploy.ts --network localhost
+echo "$(tput setaf 2)Local deployment complete. Visit localhost:3000 to view the front end"
+
