074cb94bb70ff37ca3b4f91175b949af4b5f5567
beauwilliams <beau.williams@icloud.com>
2021-12-24 14:21:49 +1100

    uncommenting...

--- DEVS-RUN-ME-FIRST.sh
+++ DEVS-RUN-ME-FIRST.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Installing deps"
-# sleep 2
-# npm install --production=false
+sleep 2
+npm install --production=false
 
 echo "Setting up dev automation scripts"
-# sleep 2
-# chmod +x ./stop.sh
-# chmod +x ./start.sh
+sleep 2
+chmod +x ./stop.sh
+chmod +x ./start.sh
 
 echo -n "Would you like to set up the aliases for this project.. \"start\" & \"stop\" (enter for y) y/n?: "
 read -r REPLY
